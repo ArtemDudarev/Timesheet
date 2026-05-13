35cdb3d2153e1d1688512549a358c2ac8ec6610a
ArtemDudarev <141909386+ArtemDudarev@users.noreply.github.com>
2026-05-13 14:13:38 +0300

    fix correction of plurals in entities (#4)

--- services/profile_service/src/models/status.py
+++ services/profile_service/src/models/status.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base
 
 class Status(Base):
-    __tablename__ = "statuses"
+    __tablename__ = "status"
 
     status_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     status_name: Mapped[str] = mapped_column(unique=True, nullable=False)
