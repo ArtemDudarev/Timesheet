eccf41b5974f3b250c1d516d21815959fb7f910d
Даниил <Oreganovegas09@mail.ru>
2026-05-18 10:41:44 +0300

    Id correction fo employee role

--- services/profile_service/src/models/employee_role.py
+++ services/profile_service/src/models/employee_role.py
@@ -6,6 +6,7 @@ from src.models.base import Base
 employee_role = Table(
     "employee_role",
     Base.metadata,
-    Column("employee_id", Uuid, ForeignKey("employee.employee_id", ondelete="CASCADE"), primary_key=True),
+    Column("employee_id", Uuid, ForeignKey("employee.id", ondelete="CASCADE"), primary_key=True),
     Column("role_id", Uuid, ForeignKey("role.id", ondelete="CASCADE"), primary_key=True),
 )
+ 
